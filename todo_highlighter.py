059d1c99abd02052b9740e2b6bc2d134a1a2980f
Zack Harley <zackharley@superna.net>
2016-06-08 13:42:42 -0400

    Basic highlighting enabled

---
+++ todo_highlighter.py
@@ -0,0 +1,13 @@
+# import sublime, sublime_plugin
+# import re
+
+# Can be run using `view.run_command('hello_world')`
+class HelloWorldCommand(sublime_plugin.EventListener):
+  def on_load(self, view):
+    mark = view.find_all(r'\/\/\s+TODO')
+    view.add_regions("mark", mark, "mark", "dot",
+            sublime.HIDDEN | sublime.PERSISTENT)
+  def on_modified(self, view):
+    mark = view.find_all(r'\/\/\s+TODO')
+    view.add_regions("mark", mark, "mark", "dot",
+            sublime.HIDDEN | sublime.PERSISTENT)
