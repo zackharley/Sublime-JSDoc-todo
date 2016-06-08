fc72b66dfe8e4302b5fa58efca673f435e82de63
Zack Harley <zackharley@superna.net>
2016-06-08 15:27:54 -0400

    Clean up code

--- todo_highlighter.py
+++ todo_highlighter.py
@@ -6,8 +6,8 @@ class HelloWorldCommand(sublime_plugin.EventListener):
   def on_load(self, view):
     mark = view.find_all(r'\/\/\s?TODO( (.+)|)')
     view.add_regions("mark", mark, "mark", "dot",
-            sublime.HIDDEN | sublime.PERSISTENT)
+      sublime.HIDDEN | sublime.PERSISTENT)
   def on_modified(self, view):
     mark = view.find_all(r'\/\/\s?TODO( (.+)|)')
     view.add_regions("mark", mark, "mark", "dot",
-            sublime.HIDDEN | sublime.PERSISTENT)
+      sublime.HIDDEN | sublime.PERSISTENT)
