c27d714f7d6361cfc704e04c0fc0c9de72169bbe
Zack Harley <zackharley@superna.net>
2016-06-08 13:52:38 -0400

    Update RexExp to only match 0 or 1 whitespace characters between '//' and  'TODO'

--- todo_highlighter.py
+++ todo_highlighter.py
@@ -4,10 +4,10 @@
 # Can be run using `view.run_command('hello_world')`
 class HelloWorldCommand(sublime_plugin.EventListener):
   def on_load(self, view):
-    mark = view.find_all(r'\/\/\s+TODO')
+    mark = view.find_all(r'\/\/\s?TODO')
     view.add_regions("mark", mark, "mark", "dot",
             sublime.HIDDEN | sublime.PERSISTENT)
   def on_modified(self, view):
-    mark = view.find_all(r'\/\/\s+TODO')
+    mark = view.find_all(r'\/\/\s?TODO')
     view.add_regions("mark", mark, "mark", "dot",
             sublime.HIDDEN | sublime.PERSISTENT)
